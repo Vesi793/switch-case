7c7edc1f59c6e04fdbb62703456ccb18488d2fc1
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-28 18:30:34 +0100

    "Fix switch case for vehicle types"

---
+++ vehicleClass/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace VehicleClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string vehicle = Console.ReadLine().ToLower();
+
+            switch (vehicle)
+            {
+                case "car":
+                case "truck":
+                case "motorcycle":
+                    Console.WriteLine("land vehicle");
+                    break;
+
+                case "boat":
+                case "ship":
+                case "submarine":
+                    Console.WriteLine("water vehicle");
+                    break;
+
+                case "airplane":
+                case "helicopter":
+                    Console.WriteLine("air vehicle");
+                    break;
+
+                    default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
