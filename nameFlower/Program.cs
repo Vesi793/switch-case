72d1700d84b739cf88deebe2cbc355509e93469c
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-28 18:24:36 +0100

    "Add flower classification program"

---
+++ nameFlower/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace flowers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string flower = Console.ReadLine().ToLower();
+
+            switch (flower)
+            {
+                case "rose":
+                case "tulip":
+                case "daisy":
+                    Console.WriteLine("Spring flower");
+                    break;
+
+                case "orchid":
+                    Console.WriteLine("Winter flower");
+                    break;
+
+                default:
+                    Console.WriteLine("unknown flower");
+                    break;
+            }
+        }
+    }
+}
