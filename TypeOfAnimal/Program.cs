5c052adb21edf84ca585bafd7fe74db3956aa0f6
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-28 18:29:23 +0100

    "Type of  anumal"

---
+++ TypeOfAnimal/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TypeOfAnimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine().ToLower();
+
+            switch (animal)
+            {
+                case "dog":
+                        Console.WriteLine("mammal");
+                    break;
+                case "snake":
+                case "crocodile":
+                case "tortoise":
+                    Console.WriteLine("reptile");
+                    break;
+
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
