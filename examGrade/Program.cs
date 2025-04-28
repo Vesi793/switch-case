c3026682563ffaf36be0ea68e882a7cc42ac8bce
Vesela Yanyova <147051362+Vesi793@users.noreply.github.com>
2025-04-28 18:25:46 +0100

    "Small corrections in exam grade project"

---
+++ examGrade/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace examGrade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int grade = int.Parse(Console.ReadLine());
+
+            switch (grade)
+            {
+                case 2:
+                    Console.WriteLine("Poor");
+                    break;
+                case 3:
+                        Console.WriteLine("Average");
+                    break;
+                case 4:
+                        Console.WriteLine("Good");
+                    break;
+                case 5:
+                        Console.WriteLine("Very good");
+                    break;
+                case 6:
+                        Console.WriteLine("Excellent");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid grade");
+                    break;
+            }
+            
+        }
+    }
+}
